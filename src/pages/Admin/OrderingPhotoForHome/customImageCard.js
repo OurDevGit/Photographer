c175408b17765d43057c502e0f193cfce5db19fa
OurDevGit <ari9411530@gmail.com>
2020-02-14 21:01:32 +0200

    add views,downloads,likes number on ordering photo

--- src/pages/Admin/OrderingPhotoForHome/customImageCard.js
+++ src/pages/Admin/OrderingPhotoForHome/customImageCard.js
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-
+import {Button, Icon, Label} from 'semantic-ui-react'
 import data from "./data.json";
 import "./style.less";
 import Board from "react-trello";
@@ -9,9 +9,39 @@ export default class customImageCard extends Component {
     render(){
         const {id, title, description, url} =  this.props;
         return (
-            <div className='Image'>
-             <img src={url} /> 
+            <div>
+                <div className='Image'>
+                <img src={url} /> 
+                </div>
+                <Button as='div' className='view ImageButton' labelPosition='right'>
+                  <Button color='gray'>
+                    <Icon name='eye' />
+                    
+                  </Button>
+                  <Label as='a' basic color='gray' pointing='left'>
+                    10
+                  </Label>
+                </Button>
+                <Button as='div' className='love ImageButton' labelPosition='right'>
+                  <Button color='red'>
+                    <Icon name='heart' />
+                    {/* Like */}
+                  </Button>
+                  <Label as='a' basic color='red' pointing='left'>
+                    20
+                  </Label>
+                </Button>
+                <Button as='div' className='download ImageButton' labelPosition='right'>
+                  <Button color='blue'>
+                    <Icon name='download' />
+                    
+                  </Button>
+                  <Label as='a' basic color='blue' pointing='left'>
+                    10
+                  </Label>
+                </Button>
             </div>
+
           );
     }
 }
